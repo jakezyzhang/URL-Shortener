44bd9f3d78851b71f4753aff3cd4fd49897b8130
zhangziyang <378659373@qq.com>
2018-04-18 09:37:20 +0800

    forth commit

--- WaterMelon/src/cn/watermelon/ssm/controller/LinkController.java
+++ WaterMelon/src/cn/watermelon/ssm/controller/LinkController.java
@@ -40,30 +40,40 @@ public class LinkController {
 	//通过长链接生成对应的短链接，访问短链接会跳转对应的长链接
 	@RequestMapping("/shortLink")
 	public String InsertLink(Model model,HttpServletRequest request,LinkCustom linkCustom,String linklong) throws Exception{
+//		System.out.println(linklong.length());
+		Boolean flag = true;
+		do{
+			if(linklong.length()==0){
+				String mistake = "网址不能为空";
+				model.addAttribute("mistake", mistake);
+				return "watermelon";
+			}else{
+				//生成短链接
+				LongToShortUrl longToShortUrl = new LongToShortUrl();
+				linkCustom.setLinklong(linklong);
+				String shortUrl = longToShortUrl.GetShortUrl(linklong);
+				
+				LinkQueryVo linkQueryVo = new LinkQueryVo();
+				linkQueryVo.setLinksCustom(linkCustom);
+				//判断数据库中是否有现在输入的长链接对应的短链接
+				if(linkService.findLinkShortUrlByLongUrl(linkQueryVo).size()==0){//如果没有对应长链接就将新得到的插入数据库
+					linkCustom.setLinkshort(shortUrl);
+					
+					linkService.LinkInsert(linkCustom);
+					
+					model.addAttribute("shortUrl", shortUrl);
+				}else if(linkService.findLinkShortUrlByLongUrl(linkQueryVo).size()!=0){//如果有数据就查出来
+					List<LinkCustom> findLinkShortUrl = linkService.findLinkShortUrlByLongUrl(linkQueryVo);
+					
+					model.addAttribute("findLinkShortUrl", findLinkShortUrl);
+				}
+				
+				model.addAttribute("LinkLongUrl", linklong);
+				flag = false;
+			}
 		
-		//生成短链接
-		LongToShortUrl longToShortUrl = new LongToShortUrl();
-		linkCustom.setLinklong(linklong);
-		String shortUrl = longToShortUrl.GetShortUrl(linklong);
+		}while(flag);
 		
-		LinkQueryVo linkQueryVo = new LinkQueryVo();
-		linkQueryVo.setLinksCustom(linkCustom);
-		//判断数据库中是否有现在输入的长链接对应的短链接
-		if(linkService.findLinkShortUrlByLongUrl(linkQueryVo).size()==0){//如果没有对应长链接就将新得到的插入数据库
-			linkCustom.setLinkshort(shortUrl);
-			
-			linkService.LinkInsert(linkCustom);
-			
-			model.addAttribute("shortUrl", shortUrl);
-		}else if(linkService.findLinkShortUrlByLongUrl(linkQueryVo).size()!=0){//如果有数据就查出来
-			List<LinkCustom> findLinkShortUrl = linkService.findLinkShortUrlByLongUrl(linkQueryVo);
-			
-			model.addAttribute("findLinkShortUrl", findLinkShortUrl);
-		}
-		
-		model.addAttribute("LinkLongUrl", linklong);
-		
-	
 		return "shortLinkUrl";
 	}
 }
